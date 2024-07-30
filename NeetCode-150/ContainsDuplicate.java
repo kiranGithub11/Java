e0cd9e6f4e284577e205dac960c3ed2e7e844fbd
@kiranGithub11 <kirannaragam@gmail.com>
2024-07-30 23:22:50 +0530

    completed contains duplicate problem

---
+++ NeetCode-150/ContainsDuplicate.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+//https://leetcode.com/problems/contains-duplicate/description/
+
+public class ContainsDuplicate {
+	// solution 1
+	static boolean containsDuplicate1(int[] nums) {
+		for(int i=0; i<nums.length-1; i++) {
+			for(int j=i+1; j<nums.length; j++) {
+				if(nums[i] == nums[j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	// solution 2
+	static boolean containsDuplicate2(int[] nums) {
+		Arrays.sort(nums);
+		for(int i=0; i<nums.length-1; i++) {
+			if(nums[i] == nums[i+1]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+    // solution 3 -- OPTIMAL --
+	static boolean containsDuplicate3(int[] nums) {
+		HashSet<Integer> hashset = new HashSet<Integer>();
+		for(int i=0; i<nums.length; i++) {
+			if(hashset.contains(nums[i])) {
+				return true;
+			} else {
+				hashset.add(nums[i]);
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 1}; // true
+        // int[] nums1 = {1, 2, 3, 4}; // false
+		System.out.println(containsDuplicate3(nums));
+	}
+}
